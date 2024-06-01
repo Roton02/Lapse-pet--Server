fe138a36668d374909408d570ab6d0710d11e27c
Roton02 <rph645102@gmail.com>
2024-06-01 21:38:19 +0600

    create a new api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -31,10 +31,27 @@ async function run() {
     await client.connect();
     const AllPeatsCategory = client.db('LafsePeats').collection('PeatsAllCategory')
     app.get('/allCategory' , async (req,res)=>{
-      const result = await AllPeatsCategory.find().toArray()
+      const searchValue = req.query.search
+      // console.log(req.query.search);
+      options = {
+        sort: { date:  -1 },
+      };
+      const searchQuery = {$regex : searchValue , $options : 'i'}
+      let query = {  };
+      if (searchValue) {
+        query={...query , name:searchQuery }
+      }
+      // console.log(query);
+      const result = await AllPeatsCategory.find(query,options).toArray()
       res.send(result)
     })
-
+    app.get("/allCategory/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await AllPeatsCategory.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
